b227018980c29c66d9f90bc99377a2424f1017e4
niangegelaile <13750523051@139.com>
2018-08-11 22:25:22 +0800

    上传图片
    Signed-off-by: niangegelaile <13750523051@139.com>

---
+++ src/main/java/cn/com/liquanan/gradleWebDemo/controller/UploadController.java
@@ -0,0 +1,42 @@
+package cn.com.liquanan.gradleWebDemo.controller;
+
+
+import cn.com.liquanan.gradleWebDemo.common.HttpResult;
+import org.apache.commons.fileupload.FileUploadException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+
+@Controller
+@RequestMapping("/upload")
+public class UploadController {
+
+    @RequestMapping("/uploadPic")
+    @ResponseBody
+    public HttpResult<Boolean> uploadPic(
+            @RequestParam MultipartFile pic)
+            throws IllegalStateException, IOException {
+        if (!pic.isEmpty()) {
+            String path = "D:\\webImage\\image\\";
+            String originalFileName = pic.getOriginalFilename();
+            // 新的图片名称
+            String newFileName = UUID.randomUUID() + originalFileName.substring(originalFileName.lastIndexOf("."));
+            // 新的图片
+            File newFile = new File(path + newFileName);
+            // 将内存中的数据写入磁盘
+            pic.transferTo(newFile);
+        }
+        HttpResult<Boolean> result=new HttpResult<>();
+        result.setResult(true);
+        return result;
+    }
+}
